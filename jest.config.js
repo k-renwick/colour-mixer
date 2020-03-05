55cf3a3dab9ce82f92296c31d75e78a357dd0d14
Katrina <krenwick@outlook.com>
2020-03-05 07:21:10 -0500

    remove extra comments in jest config

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,4 @@ module.exports = {
 
   // The test environment that will be used for testing
   testEnvironment: "node",
-
-  // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
-  // testURL: "http://localhost",
-
-  // Setting this value to "fake" allows the use of fake timers for functions such as "setTimeout"
-  // timers: "real",
 };
